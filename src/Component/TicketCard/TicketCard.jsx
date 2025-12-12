763de0a2e60c0a7e36fb7a8371aefb74ff8a31ef
jahiddeveloper <md.jahidulislam.mar@gmail.com>
2025-12-12 10:15:13 +0600

    Daynamin card style

--- src/Component/TicketCard/TicketCard.jsx
+++ src/Component/TicketCard/TicketCard.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import ellips from "../../assets/Ellipse.png";
+import ellips2 from "../../assets/Ellipse-2.png";
 import calender from "../../assets/calender.png";
 
 const TicketCard = ({ ticket }) => {
@@ -8,8 +9,18 @@ const TicketCard = ({ ticket }) => {
       <div className="bg-white shadow-sm rounded-lg p-4 cursor-pointer hover:shadow-2xl hover:transition duration-350 hover:scale-101">
         <div className="flex justify-between items-center">
           <h2 className="font-bold text-xl">{ticket.title}</h2>
-          <p className="flex items-center gap-2 px-5 py-2 font-semibold">
-            <img className="w-4 h-4 rounded-5xl" src={ellips} alt="" />
+          <p
+            className={`flex items-center gap-2 px-5 py-2 font-semibold rounded-3xl ${
+              ticket.status == "Open"
+                ? "text-[#0b5e06] bg-[#b9f8cf]"
+                : "text-[#9c7700] bg-[#f8f3b9]"
+            }`}
+          >
+            <img
+              className="w-4 h-4 rounded-5xl"
+              src={ticket.status == "Open" ? ellips : ellips2}
+              alt=""
+            />
             {ticket.status}
           </p>
         </div>
@@ -17,7 +28,17 @@ const TicketCard = ({ ticket }) => {
         <div className="flex justify-between items-center mt-4">
           <div className="flex items-center gap-2 md:gap-4">
             <p className="text-[#627382] font-semibold">{ticket.id}</p>
-            <p>{ticket.priority}</p>
+            <p
+              className={`${
+                ticket.priority == "HIGH PRIORITY"
+                  ? "text-red-500"
+                  : ticket.priority == "MEDIUM PRIORITY"
+                  ? "text-[#febb0c]"
+                  : "text-[#02a53b]"
+              }`}
+            >
+              {ticket.priority}
+            </p>
           </div>
           <div className="flex items-center gap-2 md:gap-4">
             <p className="text-[#627382] font-semibold">{ticket.customer}</p>
