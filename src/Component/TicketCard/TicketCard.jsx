062d574fd282d2f5d543cc321b9c502dd8d8995f
jahiddeveloper <md.jahidulislam.mar@gmail.com>
2025-12-12 09:51:48 +0600

    Dayanamic card design compleat

--- src/Component/TicketCard/TicketCard.jsx
+++ src/Component/TicketCard/TicketCard.jsx
@@ -2,33 +2,28 @@ import React from "react";
 import ellips from "../../assets/Ellipse.png";
 import calender from "../../assets/calender.png";
 
-const TicketCard = ({ticket}) => {
+const TicketCard = ({ ticket }) => {
   return (
     <div>
-      <div className="bg-white shadow-sm rounded-lg p-4 cursor-pointer hover:shadow-2xl hover:transition duration-350">
+      <div className="bg-white shadow-sm rounded-lg p-4 cursor-pointer hover:shadow-2xl hover:transition duration-350 hover:scale-101">
         <div className="flex justify-between items-center">
-          <h2 className="font-bold text-xl">
-            Login Issues - Can't Access Account
-          </h2>
+          <h2 className="font-bold text-xl">{ticket.title}</h2>
           <p className="flex items-center gap-2 px-5 py-2 font-semibold">
             <img className="w-4 h-4 rounded-5xl" src={ellips} alt="" />
-            Open
+            {ticket.status}
           </p>
         </div>
-        <p className="mt-3 text-[#627382]">
-          Customer is unable to log in to their account. They've tried resetting
-          their password multiple times but still...
-        </p>
+        <p className="mt-3 text-[#627382] md:w-125">{ticket.description}</p>
         <div className="flex justify-between items-center mt-4">
-          <div className="flex items-center gap-4">
-            <p className="text-[#627382] font-semibold">#1001</p>
-            <p>HIGH PRIORITY</p>
+          <div className="flex items-center gap-2 md:gap-4">
+            <p className="text-[#627382] font-semibold">{ticket.id}</p>
+            <p>{ticket.priority}</p>
           </div>
-          <div className="flex items-center gap-4">
-            <p className="text-[#627382] font-semibold">Nmae</p>
-            <div className="flex items-center gap-3">
+          <div className="flex items-center gap-2 md:gap-4">
+            <p className="text-[#627382] font-semibold">{ticket.customer}</p>
+            <div className="flex items-center gap-1 md:gap-3">
               <img src={calender} alt="" />
-              <p>1/15/2024</p>
+              <p>{ticket.date}</p>
             </div>
           </div>
         </div>
--- src/Component/TicketsCard/TicketsCard.jsx
+++ src/Component/TicketsCard/TicketsCard.jsx
@@ -1,21 +1,20 @@
 import React, { use } from "react";
 import TicketCard from "../TicketCard/TicketCard";
 
-
-const TicketsCard = ({fetchTicket}) => {
-    let ticket = use(fetchTicket)
+const TicketsCard = ({ fetchTicket }) => {
+  let ticket = use(fetchTicket);
   return (
     <div className="md:w-11/12 mx-auto mt-10 md:mt-20 grid grid-cols-1 md:grid-cols-12 gap-4">
-      <div className="col-span-8">
-        <h2 className="font-bold text-2xl">Customer Tickets</h2>
-        <div className="mt-4">
-          {
-            ticket.map(ticket => <TicketCard key={ticket.id} ticket = {ticket}></TicketCard>)
-          }
+      <div className="col-span-9">
+        <h2 className="font-bold text-2xl mx-2 md:mx-auto">Customer Tickets</h2>
+        <div className="mt-4 grid grid-cols-1 md:grid-cols-2 gap-4 mx-2 md:mx-auto">
+          {ticket.map((ticket) => (
+            <TicketCard key={ticket.id} ticket={ticket}></TicketCard>
+          ))}
         </div>
       </div>
 
-      <div className="col-span-4 bg-green-200">j</div>
+      <div className="col-span-3 bg-green-200">j</div>
     </div>
   );
 };
