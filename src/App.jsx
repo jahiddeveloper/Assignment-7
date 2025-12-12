68d17bd238dcdcaf3813b4a3e829074df74798f5
jahiddeveloper <md.jahidulislam.mar@gmail.com>
2025-12-12 08:05:40 +0600

    Footer added

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import './App.css'
+import Footer from './Component/Footer'
 import Hero from './Component/Hero'
 import Navbar from './Component/Navbar'
 
@@ -8,6 +9,7 @@ function App() {
     <>
       <Navbar></Navbar>
       <Hero></Hero>
+      <Footer></Footer>
     </>
   )
 }
