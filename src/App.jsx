56a9722203958722cf54e00c396e926165aa76ac
jahiddeveloper <md.jahidulislam.mar@gmail.com>
2025-12-12 08:02:08 +0600

    Hero section added

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import './App.css'
+import Hero from './Component/Hero'
 import Navbar from './Component/Navbar'
 
 function App() {
@@ -6,6 +7,7 @@ function App() {
   return (
     <>
       <Navbar></Navbar>
+      <Hero></Hero>
     </>
   )
 }
